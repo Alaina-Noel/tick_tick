147133a223d2fbc5ee289256178bf3f465981023
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-12-08 10:28:24 -0700

    added multiply buttons

--- src/App/App.js
+++ src/App/App.js
@@ -3,11 +3,28 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
+  const [product, setProduct] = useState(1);
   return (
+    <div>
     <div className="App">
       <p>{count}</p>
-      <button onClick={() => setCount(count + 1)}>Add One</button>
+      <button onClick={() => {
+        setCount(count + 1);
+        setCount2(count + 1);
+        } 
+      }>Add One</button>
       <button onClick={() => setCount(count - 1)}>Subtract One</button>
+    </div>
+    <div>
+    <p>{count2}</p>
+    <button onClick={() => setCount2(count2 + 1)}>Add One</button>
+    <button onClick={() => setCount2(count2 - 1)}>Subtract One</button>
+    </div>
+    <div>
+    <p>{product}</p>
+    <button onClick={() => setProduct(count * count2)}>Multiply Nums</button>
+    </div>
     </div>
   );
 }
